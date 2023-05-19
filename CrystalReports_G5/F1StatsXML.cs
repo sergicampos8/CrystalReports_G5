867c56a2a258764d4efd8b9a67e473b1e3257656
bescoss <pablo.bescos@sarria.salesians.cat>
2023-05-19 20:13:06 +0200

    MERGE Y TAL

--- CrystalReports_G5/F1StatsXML.cs
+++ CrystalReports_G5/F1StatsXML.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -14,9 +15,16 @@ namespace CrystalReports_G5
 {
     public partial class F1StatsXML : Form
     {
+        static FuncionesAuxiliares funcionesAuxiliares = new FuncionesAuxiliares();
+        static ArrayList ArrayDrivers = new ArrayList();
+        static ArrayList ArrayRTeams = new ArrayList();
+        static ArrayList ArrayGP = new ArrayList();
         public F1StatsXML()
         {
             InitializeComponent();
+
+
+   
         }
 
         private void label2_Click(object sender, EventArgs e)
@@ -26,14 +34,14 @@ namespace CrystalReports_G5
 
         private void LoadButton_Click(object sender, EventArgs e)
         {
+            ArrayList lines = new ArrayList();
             string filePath = FileBox.Text.Trim();
-            FuncionesAuxiliares funcionesAuxiliares = new FuncionesAuxiliares();
-            funcionesAuxiliares.ReadFile(filePath);
+            lines = funcionesAuxiliares.ReadFile(filePath);
+            funcionesAuxiliares.FillArraylists(lines);
         }
 
         private void ButtonBrowse_Click(object sender, EventArgs e)
         {
-            FuncionesAuxiliares funcionesAuxiliares = new FuncionesAuxiliares();
             string rutaArchivo = funcionesAuxiliares.BrowseFile();
             FileBox.Text = rutaArchivo;
 
